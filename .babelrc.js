8132e70f76a3510f17579bc9bcd951ca5fc0a5aa
Bin Hou <houbin217jz@gmail.com>
2019-10-02 18:34:18 +0900

    v1.3.2 Update deps

--- .babelrc.js
+++ .babelrc.js
@@ -2,13 +2,19 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
-      {
-        targets: {
-          browsers: ['ie >= 11']
-        },
-        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
-        loose: true
-      }
+      process.env.NODE_ENV === 'test'
+        ? {
+            targets: {
+              node: 'current'
+            }
+          }
+        : {
+            targets: {
+              browsers: ['ie >= 11']
+            },
+            modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
+            loose: true
+          }
     ]
   ],
   plugins: ['@babel/plugin-proposal-object-rest-spread']
